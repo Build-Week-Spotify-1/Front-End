6079de04e5066f64cb31ade40bc3cee09ee618c0
Cody Denniston <dennistonc4817@gapps.bcsds.org>
2020-03-03 13:08:19 -0600

    working on styling

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,17 +1,5 @@
 import React from 'react';
-import Avatar from '@material-ui/core/Avatar';
-import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
-import Link from '@material-ui/core/Link';
-import Grid from '@material-ui/core/Grid';
-import Box from '@material-ui/core/Box';
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
-import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
-import Container from '@material-ui/core/Container';
+import {Avatar, Button, CssBaseline, TextField, FormControlLabel, Checkbox, Link, Grid, Box, Typography, makeStyles, Container} from '@material-ui/core/';
 import styled from "styled-components";
 
 
@@ -27,6 +15,11 @@ function Copyright() {
     </Typography>
   );
 }
+
+const MyContainer = styled(Container)`
+background: green;
+margin: -200px, 0;
+`
 
 const useStyles = makeStyles(theme => ({
   paper: {
@@ -52,6 +45,7 @@ export default function SignIn() {
   const classes = useStyles();
 
   return (
+     
     <Container component="main" maxWidth="xs">
       <CssBaseline />
       <div className={classes.paper}>
@@ -115,5 +109,6 @@ export default function SignIn() {
         <Copyright />
       </Box>
     </Container>
+   
   );
 }
