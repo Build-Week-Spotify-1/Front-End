63be86ef8670260b2411672b500aa7c46d28d215
david-pok <david.pok@gmail.com>
2020-03-03 14:49:27 -0800

    adds redux statemanagement

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Front-End
 
 [Product Vision Document](https://docs.google.com/document/d/1mfx4VSyZvkC7BVNo1Vz5nHS8Sx7mu_JJmQVh0p0T9o8/)
+
+Use NPM for dependencies
---
+++ src/actions/SignupAction.js
@@ -0,0 +1,19 @@
+import AxiosWithAuth from "../utils/AxiosWithAuth";
+
+export const SIGNUP_START = "SIGNUP_START";
+export const SIGNUP_SUCCESS = "SIGNUP_SUCCESS";
+export const SIGNUP_FAIL = "SIGNUP_FAIL";
+
+export const signup = info => {
+  return dispatch => {
+    dispatch({ type: SIGNUP_START });
+    console.log(info);
+    AxiosWithAuth()
+      .post("/api/auth/register", info)
+      .then(res => {
+        console.log("signup res", res);
+        dispatch({ type: SIGNUP_SUCCESS, payload: res.data });
+      })
+      .catch(err => console.log(err.res));
+  };
+};
