add564068e439fbeafe91fdbcf542a76d1900f4c
Cody Denniston <dennistonc4817@gapps.bcsds.org>
2020-03-04 13:31:51 -0600

    Edit Profile started

--- src/components/EditProfile.js
+++ src/components/EditProfile.js
@@ -1,1 +1,50 @@
 import React from "react"
+
+export default function EditProfile() {
+
+    return (
+        <section className="edit-profile">
+            <header>
+                <nav>
+                    <a href="/"> Home </a>
+                </nav>
+            </header>
+            <h1>
+                Edit Profile
+            </h1>
+            <form className="input-sections">
+                <label className="labels">
+                    <h4>Name:</h4>
+                    <input
+                    id="name"
+                    type="text"
+                    name="name"
+                    placeholder="name"
+                    />
+                </label>
+                <label className="labels">
+                    <h4>Username</h4>
+                    <input
+                    id="username"
+                    type="text"
+                    name="username"
+                    placeholder="username"
+                    />
+                </label>
+                <label className="labels">
+                    <h4>Password</h4>
+                    <input
+                    id="password"
+                    type="text"
+                    name="password"
+                    placeholder="password"
+                    />
+                </label>
+            </form>
+            <footer>
+                copyright
+            </footer>
+        </section>
+    )
+
+}
